bd09099fb7d98665e2aedec5f5b1f676c525acfe
loordbarringtn <55858015+loordbarringtn@users.noreply.github.com>
2019-10-07 00:09:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,13 @@ import os
 import argparse
 
 def show_total_clicks_bitly(bitly_token,url):
-    try:
       api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary'
       headers = {"Authorization": "Bearer " + bitly_token}
       response = requests.get(api_url, headers=headers)
       response.raise_for_status()
       return response.text
-    except requests.exceptions.HTTPError as errh:
-        return "An Http Error occurred:" + repr(errh)
-    except requests.exceptions.ConnectionError as errc:
-        return "An Error Connecting to the API occurred:" + repr(errc)
-    except requests.exceptions.Timeout as errt:
-        return "A Timeout Error occurred:" + repr(errt)
-    except requests.exceptions.RequestException as err:
-        return "An Unknown Error occurred" + repr(err)
 
 def short_links_bitly(bitly_token, url):
-    try:
       api_url='https://api-ssl.bitly.com/v4/bitlinks'
       headers = {"Authorization":"Bearer"}
       headers['Authorization'] = headers['Authorization'] + " " + bitly_token
@@ -29,14 +19,6 @@ def short_links_bitly(bitly_token, url):
       response = requests.post(api_url, json=payload, headers=headers)
       response.raise_for_status()
       return response.json()
-    except requests.exceptions.HTTPError as errh:
-      return "An Http Error occurred:" + repr(errh)
-    except requests.exceptions.ConnectionError as errc:
-        return "An Error Connecting to the API occurred:" + repr(errc)
-    except requests.exceptions.Timeout as errt:
-        return "A Timeout Error occurred:" + repr(errt)
-    except requests.exceptions.RequestException as err:
-        return "An Unknown Error occurred" + repr(err)
 
 def main():
     load_dotenv()
@@ -45,10 +27,19 @@ def main():
     parser.add_argument("website_url", help="Введите web адрес",type=str)
     args = parser.parse_args()
     url=args.website_url
-    if url.startswith('bit.ly'):
-       print(show_total_clicks_bitly(bitly_token,url))
-    else:
-       print(short_links_bitly(bitly_token,url))
+    try:
+        if url.startswith('bit.ly'):
+           print(show_total_clicks_bitly(bitly_token,url))
+        else:
+           print(short_links_bitly(bitly_token,url))
+    except requests.exceptions.HTTPError as errh:
+        print  ("An Http Error occurred:" + repr(errh))
+    except requests.exceptions.ConnectionError as errc:
+        print ("An Error Connecting to the API occurred:" + repr(errc))
+    except requests.exceptions.Timeout as errt:
+        print ("A Timeout Error occurred:" + repr(errt))
+    except requests.exceptions.RequestException as err:
+        print ("An Unknown Error occurred" + repr(err))
 
 if __name__ == '__main__':
     main()
